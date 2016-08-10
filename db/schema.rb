97033967eba196d5ead4f9d98f7e40a44863a15c
sonia02goplani <sonia.goplani779@gmail.com>
2016-08-10 13:25:51 +0530

    using postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160810070601) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "coustomers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -29,16 +32,7 @@ ActiveRecord::Schema.define(version: 20160810070601) do
     t.datetime "updated_at"
   end
 
-  add_index "coustomers", ["email"], name: "index_coustomers_on_email", unique: true
-  add_index "coustomers", ["reset_password_token"], name: "index_coustomers_on_reset_password_token", unique: true
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "coustomers", ["email"], name: "index_coustomers_on_email", unique: true, using: :btree
+  add_index "coustomers", ["reset_password_token"], name: "index_coustomers_on_reset_password_token", unique: true, using: :btree
 
 end
